8a3b55aee4ffd7c0181d05bad2bc2e94abb29c1f
chrisziegler <chrisrziegler@gmail.com>
2017-09-22 15:51:34 -0700

    added list (getAll()) and logNote() methods

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,7 +1,5 @@
-console.log('starting app.js...');
-
-const fs = require('fs');
-const _ = require('lodash');
+// const fs = require('fs');
+// const _ = require('lodash'); 
 const yargs = require('yargs');
 
 const notes = require('./notes.js');
@@ -9,18 +7,42 @@ const notes = require('./notes.js');
 const argv = yargs.argv;
 // var command = process.argv[2];
 const command = argv._[0];
-console.log('Command: ', command);
-console.log('Process: ', process.argv);
-console.log('Yargs: ', argv);
+
+// console.log('Command: ', command);
+// console.log('Process: ', process.argv);
+// console.log('Yargs: ', argv);
 
 if(command === 'add') {
-    notes.addNote(argv.title, argv.body)
+     let note = notes.addNote(argv.title, argv.body);
+     if (note) {
+        console.log('Note created');
+        notes.logNote(note);
+    } else {
+        console.log('title already in use');
+    }
 } else if (command === 'list') {
-    notes.getAll();
+    //since not returns an array of objects, instead of single object
+    //cannot simply combine both functions with logNote()
+    let note = notes.getAll();
+    console.log('List of notes\n--------------------------');
+    for (let prop in note) {
+        let item = note[prop]; //so note[0], note[1], etc
+        console.log(`title: ${item.title}, body: ${item.body}`);
+    }
 } else if (command === 'read') {
-    notes.getNote(argv.title);
+    let note = notes.getNote(argv.title);
+    if (note) {
+        console.log('Note found');
+        notes.logNote(note);
+    } else {
+        console.log('Note not found');
+    }
+    
 } else if (command === 'remove') {
-    notes.removeNote(argv.title);
+    let noteRemoved = notes.removeNote(argv.title);
+    let message = noteRemoved ? 'Note was removed' : 'Note not found';
+    console.log(message);
 } else {
     console.log('command not recognized')
 }
+
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,23 +1,83 @@
 
+const fs = require('fs');
+
+const fetchNotes = () => {
+    try {
+        const notesString = fs.readFileSync(__dirname +'/notes-data.json');
+        return JSON.parse(notesString);
+    } catch(e) {
+        return [];
+    }
+};
+
+const saveNotes = (notes) => {
+    fs.writeFileSync(`${__dirname}/notes-data.json`, JSON.stringify(notes))
+};
+
 const addNote = (title, body) => {
-    console.log('Adding note ', title, body);
+    let notes = fetchNotes();
+    const note = {
+        title,
+        body
+    }
+    
+    const duplicateNotes = notes.filter((note) => note.title === title);
+    if (duplicateNotes.length === 0) {
+        notes.push(note);
+        saveNotes(notes);
+        return note;
+    } 
 };
 const getAll = () => {
-    console.log('Getting all notes.')
+    return fetchNotes();
 }
 const getNote = (title) => {
-    console.log('Read a note.', title)
+    let notes = fetchNotes();
+    let findNote = notes.find(note => note.title === title)
+    return findNote;
 }
-const removeNote = (title) => {
-    console.log('Delete a specific note.', title)
-}
+const removeNote = (title) =>{
+ let notes = fetchNotes();
+ console.log(notes);
+ let filteredNotes = notes.filter((note) => note.title !== title);
+ console.log(filteredNotes);
+ saveNotes(filteredNotes);
+ return notes.length !== filteredNotes.length;
+};
 
+const logNote = (note) => {
+    // //use 1st section if  logNote to handle both read and list
+    // // console.log(note.length)
+    // //use lodash to check type - if array of objects, or single object?
+    // console.log(note)
+    // // when using list command to getAll - multiple titles - note returns
+    // // [ { title: 'note7', body: 'body7' },
+    // // { title: 'note8', body: 'body8' },
+    // // { title: 'hello', body: 'world' } ]
+    // //ADD THIS EXAMPLE TO EXERCISM ITERATING OBJECTS/JSON ASIDE
+    // console.log('List of notes\n--------------------------');
+    // for (let prop in note) {
+    //     let item = note[prop]; //so note[0], note[1], etc
+    //     //just selecting the objects with this one
+    //     //get the values off each item contained on the objects in the array we're
+    //     //iterating through. Better without the 'title' & 'body' labels here.
+    //     console.log(`title: ${item.title}, body: ${item.body}`);
+    // }
+
+
+    //complete old code
+    console.log('----------------');
+    console.log(`Title: ${note.title}`);
+    console.log(`Body: ${note.body}`);
+}
 
 module.exports = { 
     addNote,
     getAll,
     getNote,
-    removeNote
+    removeNote,
+    logNote
 }
+
 
 
